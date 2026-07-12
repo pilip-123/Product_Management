fc25e5915da42ac68667a5fcc77e5f3ca955730e
pilip-123 <pilipyom985@gmail.com>
2026-07-12 13:41:56 +0700

    test

--- Frontend/src/components/HelloWorld.vue
+++ Frontend/src/components/HelloWorld.vue
@@ -11,10 +11,11 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      You’ve successfully created a project 
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
+    <h1>Hello welcome to our Products</h1>
   </div>
 </template>
 
