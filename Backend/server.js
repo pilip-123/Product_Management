3d8dd02ab56e20b24a7feff52d99980255cb6dc9
Pilip yom <pilipyom985@gmail.com>
2026-09-10 08:57:53 +0700

    fix error and add cors to backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,16 @@
 require('dotenv/config');
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./src/routers/userRoute');
 const productRouter = require('./src/routers/productRoute');
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use('/api', userRouter);
